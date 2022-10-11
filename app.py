d24a1324a51333426f94831f1c52784d41bfc28b
Jaianviz <115568492+Jaianviz@users.noreply.github.com>
2022-10-11 20:32:57 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('D://ML ENGINEER//Machine Learning//model.pkl', 'rb'))
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    float_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(float_features)]
+    prediction = model.predict(final_features) 
+
+    if prediction == 1:
+        pred = "Congrtas!, Customer is eligible for House Loan"
+    elif prediction == 0:
+        pred = "Sorry, Customer is Not eligible.Better to try next time"
+    output = pred
+
+    return render_template('result.html', prediction_text='{}'.format(output))
+
+if __name__ == "__main__":
+    app.run(debug=True)
